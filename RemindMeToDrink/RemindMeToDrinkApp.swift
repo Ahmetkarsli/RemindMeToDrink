32e48a72f0fbca2452547b82329f46fa082bb653
ahmetkarsli <ahmet.karsli@icloud.com>
2024-08-08 00:48:24 +0200

    Initial Commit

---
+++ RemindMeToDrink/RemindMeToDrinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindMeToDrinkApp.swift
+//  RemindMeToDrink
+//
+//  Created by Ahmet Karsli on 08.08.24.
+//
+
+import SwiftUI
+
+@main
+struct RemindMeToDrinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
